a6cabd5adbf9b64758048502e2cde4d806e51c31
Prachi-Sanyal <161684030+Prachi-Sanyal@users.noreply.github.com>
2024-09-25 16:48:59 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-
-// Database configuration
-$servername = "localhost";
-$username = "root"; // Default username for XAMPP
-$password = ""; // Default password for XAMPP
-$dbname = "money_manager";
-?>
